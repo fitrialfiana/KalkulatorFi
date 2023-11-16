6262426141f36ae925444f6b0b2e54f2f763a262
user <user@DESKTOP-05HFO74>
2023-11-16 17:28:48 +0700

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalkulatorFi
+{
+    public class Calculator
+    {
+        public int Tambah(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Kurang(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Bagi(int a, int b)
+        {
+            return a / b;
+        }
+
+        public int Kali(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Modulus(int a, int b)
+        {
+            return a % b;
+        }
+    }
+}
